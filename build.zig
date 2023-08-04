1b01b4245dd13bbe05f79ab90207ea8f884277af
Stephen Gutekanst <stephen@hexops.com>
2023-08-03 18:34:17 -0700

    implement workaround for hexops/mach#903
    Thanks to @mitchellh for the inspiration and letting me borrow some
    code!
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ build.zig
@@ -0,0 +1,109 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "stub",
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.linkLibC();
+    lib.addCSourceFiles(&.{"stub.c"}, &.{});
+
+    repository_url = "https://github.com/hexops/xcode-frameworks";
+    addPaths(b, lib);
+
+    b.installArtifact(lib);
+}
+
+pub var repository_url = "https://github.com/hexops/xcode-frameworks";
+
+pub fn addPaths(b: *std.Build, step: *std.build.CompileStep) void {
+    xEnsureGitRepoCloned(b.allocator, repository_url, "723aa55e9752c8c6c25d3413722b5fe13d72ac4f", xSdkPath("/zig-cache/xcode_frameworks")) catch |err| @panic(@errorName(err));
+
+    step.addFrameworkPath(.{ .path = xSdkPath("/zig-cache/xcode_frameworks/Frameworks") });
+    step.addSystemIncludePath(.{ .path = xSdkPath("/zig-cache/xcode_frameworks/include") });
+    step.addLibraryPath(.{ .path = xSdkPath("/zig-cache/xcode_frameworks/lib") });
+}
+
+fn xEnsureGitRepoCloned(allocator: std.mem.Allocator, clone_url: []const u8, revision: []const u8, dir: []const u8) !void {
+    if (xIsEnvVarTruthy(allocator, "NO_ENSURE_SUBMODULES") or xIsEnvVarTruthy(allocator, "NO_ENSURE_GIT")) {
+        return;
+    }
+
+    xEnsureGit(allocator);
+
+    if (std.fs.openDirAbsolute(dir, .{})) |_| {
+        const current_revision = try xGetCurrentGitRevision(allocator, dir);
+        if (!std.mem.eql(u8, current_revision, revision)) {
+            // Reset to the desired revision
+            xExec(allocator, &[_][]const u8{ "git", "fetch" }, dir) catch |err| std.debug.print("warning: failed to 'git fetch' in {s}: {s}\n", .{ dir, @errorName(err) });
+            try xExec(allocator, &[_][]const u8{ "git", "checkout", "--quiet", "--force", revision }, dir);
+            try xExec(allocator, &[_][]const u8{ "git", "submodule", "update", "--init", "--recursive" }, dir);
+        }
+        return;
+    } else |err| return switch (err) {
+        error.FileNotFound => {
+            std.log.info("cloning required dependency..\ngit clone {s} {s}..\n", .{ clone_url, dir });
+
+            try xExec(allocator, &[_][]const u8{ "git", "clone", "-c", "core.longpaths=true", clone_url, dir }, ".");
+            try xExec(allocator, &[_][]const u8{ "git", "checkout", "--quiet", "--force", revision }, dir);
+            try xExec(allocator, &[_][]const u8{ "git", "submodule", "update", "--init", "--recursive" }, dir);
+            return;
+        },
+        else => err,
+    };
+}
+
+fn xExec(allocator: std.mem.Allocator, argv: []const []const u8, cwd: []const u8) !void {
+    var child = std.ChildProcess.init(argv, allocator);
+    child.cwd = cwd;
+    _ = try child.spawnAndWait();
+}
+
+fn xGetCurrentGitRevision(allocator: std.mem.Allocator, cwd: []const u8) ![]const u8 {
+    const result = try std.ChildProcess.exec(.{ .allocator = allocator, .argv = &.{ "git", "rev-parse", "HEAD" }, .cwd = cwd });
+    allocator.free(result.stderr);
+    if (result.stdout.len > 0) return result.stdout[0 .. result.stdout.len - 1]; // trim newline
+    return result.stdout;
+}
+
+fn xEnsureGit(allocator: std.mem.Allocator) void {
+    const argv = &[_][]const u8{ "git", "--version" };
+    const result = std.ChildProcess.exec(.{
+        .allocator = allocator,
+        .argv = argv,
+        .cwd = ".",
+    }) catch { // e.g. FileNotFound
+        std.log.err("mach: error: 'git --version' failed. Is git not installed?", .{});
+        std.process.exit(1);
+    };
+    defer {
+        allocator.free(result.stderr);
+        allocator.free(result.stdout);
+    }
+    if (result.term.Exited != 0) {
+        std.log.err("mach: error: 'git --version' failed. Is git not installed?", .{});
+        std.process.exit(1);
+    }
+}
+
+fn xIsEnvVarTruthy(allocator: std.mem.Allocator, name: []const u8) bool {
+    if (std.process.getEnvVarOwned(allocator, name)) |truthy| {
+        defer allocator.free(truthy);
+        if (std.mem.eql(u8, truthy, "true")) return true;
+        return false;
+    } else |_| {
+        return false;
+    }
+}
+
+fn xSdkPath(comptime suffix: []const u8) []const u8 {
+    if (suffix[0] != '/') @compileError("suffix must be an absolute path");
+    return comptime blk: {
+        const root_dir = std.fs.path.dirname(@src().file) orelse ".";
+        break :blk root_dir ++ suffix;
+    };
+}
